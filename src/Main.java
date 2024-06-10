c2b3d2bec52d1d2af87e8066bdcbb3fe61d232ac
DanyilPustobaiev <11pustobaev.d@gmail.com>
2024-06-10 18:41:18 +0200

    Add conversion method km to miles

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("App for converting distance measurement.");
         double m = 1;
+        double kilometer = 1;
         double km = convMilesToKm(m);
+        double mile = convKmtoMile(kilometer);
         System.out.println("From miles to km. Result is: " + km + "km");
+        System.out.println("From km to miles. Result is: " + mile + "miles");
     }
     private static double convMilesToKm(double m) {
         return m * CONV_M;
+    }
+    private static double convKmtoMile(double kilometer) {
+        return kilometer / CONV_M;
     }
 }
